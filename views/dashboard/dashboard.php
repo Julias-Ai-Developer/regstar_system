dbc61224377e84163a3b8af57826464408f2024b
Julias-Ai-Developer <muyambijulias@gmail.com>
2025-08-01 22:13:52 +0300

    Resolved conflict

--- views/dashboard/dashboard.php
+++ views/dashboard/dashboard.php
@@ -1,39 +1,10 @@
-<<<<<<< HEAD
-<?php include '../../includes/header.php'?>
-    <div class="sidebar-overlay" id="sidebarOverlay"></div>
-<!-- ---sidebar.... -->
-<?php include '../../includes/sidebar.php'?>
-<?php include '../../includes/topnav.php'?>
-=======
 <?php include '../../includes/header.php' ?>
+<!-- ---sidebar.... -->
 <?php include '../../includes/sidebar.php' ?>
-<?php include '../../includes/navbar.php' ?>
-<!-- Body main section starts -->
-<main>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                
->>>>>>> 2003c35d5c4623d0e348d5ce8e0168c04b6fbf26
+<?php include '../../includes/topnav.php' ?>
+<!-- put main content here -->
+<div class="container-fluid p-4">
 
-        <div class="container-fluid p-4">
-           
-
-        
-        </div>
-<<<<<<< HEAD
-=======
-    </div>
-</main>
-</div>
-</div>
-<!-- Body main section ends -->
-<!-- tap on top -->
-<div class="go-top">
-    <span class="progress-value">
-        <i class="ti ti-arrow-up"></i>
-    </span>
 </div>
 
->>>>>>> 2003c35d5c4623d0e348d5ce8e0168c04b6fbf26
-<?php include '../../includes/footer.php'?>
+<?php include '../../includes/footer.php' ?>
